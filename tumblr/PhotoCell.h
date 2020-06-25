2a4813bb5b5d81671c4997f0b814f537b667545b
Ana Cismaru <anacismaru@gmail.com>
2020-06-25 09:48:50 -0700

    created basic tumblr image viewing app

---
+++ tumblr/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  tumblr
+//
+//  Created by Ana Cismaru on 6/25/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ tumblr/PhotoCell.m
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.m
+//  tumblr
+//
+//  Created by Ana Cismaru on 6/25/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
